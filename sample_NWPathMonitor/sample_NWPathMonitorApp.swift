9b87e2a04fca97d1c0224a00e035866d26c27404
AppClimber <mngw0315@gmail.com>
2023-04-22 21:41:18 +0900

    Initial Commit

---
+++ sample_NWPathMonitor/sample_NWPathMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_NWPathMonitorApp.swift
+//  sample_NWPathMonitor
+//
+//  Created by Masahiko Minagawa on 2023/04/22.
+//
+
+import SwiftUI
+
+@main
+struct sample_NWPathMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
